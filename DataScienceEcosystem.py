48f3f447d65548c4306d2d10b1233685afdbd13b
ARichards25 <112968965+ARichards25@users.noreply.github.com>
2023-07-19 14:41:15 -0500

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo">
+# </center>
+# 
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, data science tools and ecosystems are summarized. 
+
+# In[ ]:
+
+
+Some of the popular languages that data scientists use are: 
+1. Python
+2. SQL
+3. R
+
+
+# Some of the commonly used libraries by data scientists include: 
+# 1. NumPy
+# 2. DplyR
+# 3. GGPlot
+# 4. StringR
+
+# |Data Science Tools|
+# |----------------------|
+# |RStudio|
+# |Apache Spark|
+# |Github|
+
+# Below are a few examples of evaluating arithmetic expressions in Python. 
+
+# In[2]:
+
+
+##This is a simple arithmetic example to multiply then add integers. 
+x=3
+y=4
+z=5
+num = (x*y)+z
+print(num)
+
+
+# In[4]:
+
+
+##This will convert 200 minutes to hours by dividing by 60. 
+min=200
+hours=min/60
+print(hours)
+
+
+# Objectives: 
+# - Languages include R, Python and SQL
+# - Packages are used to perform functions in languages
+# - Many options for platforms, both free and paid include RStudio, JupyterLab, Github
+
+# ## Author:
+# Amanda Richards
